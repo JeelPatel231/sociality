fb7be9b955ad61508e9d4f3473dbb6c82023e5d5
jeelpatel231 <jeelpatel231@gmail.com>
2024-06-23 19:32:49 +0530

    fix input type hint

--- bot/filters/regex_filter.py
+++ bot/filters/regex_filter.py
@@ -2,7 +2,7 @@ from pyrogram.filters import create
 from pyrogram.types import Message
 import re
 
-def create_regex_filter(*patterns: re.Pattern[str]):
+def create_regex_filter(*patterns: str):
   async def regex_filter(_, __, m: Message):
     compiled = map(re.compile, patterns)
     return m.text and any(map(lambda x: bool(x.match(m.text)), compiled))
