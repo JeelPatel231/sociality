7dd3a7774dd2415c6be35d6411dfc58e857b1a2f
jeelpatel231 <jeelpatel231@gmail.com>
2024-06-23 19:34:17 +0530

    make the hook only an adapter for async
    refactor all other bot logic to bot message handler

--- bot/lib/gallery_dl_hook.py
+++ bot/lib/gallery_dl_hook.py
@@ -1,11 +1,6 @@
-import os
 import asyncio
-import mimetypes
-from typing import Any
 from collections import defaultdict
-from pyrogram.types.messages_and_media import Message
 from gallery_dl.job import Job
-from pyrogram.types import InputMediaPhoto, InputMediaVideo
 from gallery_dl.path import PathFormat
 from gallery_dl.postprocessor.common import PostProcessor
 from .gallery_dl_config import setup
@@ -14,10 +9,11 @@ setup() #this will setup gallery dl config
 
 class UploadGroupPostProcessor(PostProcessor):
 
-    def __init__(self, job: Job, msg: Message, options=None):
+    def __init__(self, job: Job, options=None):
       PostProcessor.__init__(self, job)
-      self.__msg = msg
+      self.__job = job
       self.__file_paths = []
+      self.__fut = asyncio.get_running_loop().create_future()
       ###
       job.hooks = defaultdict(list)
       job.register_hooks({
@@ -25,29 +21,12 @@ class UploadGroupPostProcessor(PostProcessor):
       }, options)
       job.hooks["finalize"].append(self.finalize)
 
-    def __map_to_telegram(self, path: str):
-      (_type, _encoding) = mimetypes.guess_type(path)
-      if _type.startswith('image'):
-        return InputMediaPhoto(path)
-      
-      if _type.startswith('video'):
-        return InputMediaVideo(path)
-
-      raise Exception(f'Unhandled Mime Type: {_type} : {_encoding}')
-    
-    def __clean_files(self, x: Any):
-      print(x)
-      for i in self.__file_paths:
-        os.remove(i)
-    
     def file_hook(self, pathfmt: PathFormat):
       self.__file_paths.append(pathfmt.path)
 
     def finalize(self, pathfmt: PathFormat):
-      print(f'finalizing for {self.__msg.text}')
-      media_group = list(map(self.__map_to_telegram, self.__file_paths))
-      print(f'replying for {self.__msg.text}')
-      coroutine = self.__msg.reply_media_group(media=media_group, quote=True)
-      fut = asyncio.run_coroutine_threadsafe(coroutine, asyncio.get_running_loop())
-      fut.add_done_callback(lambda x: self.__clean_files(x))
+      self.__fut.set_result(self.__file_paths)
 
+    def job_result(self) ->  asyncio.Future[list[str]]:
+      self.__job.run()
+      return self.__fut
--- bot/plugins/instagram.py
+++ bot/plugins/instagram.py
@@ -1,7 +1,10 @@
+import mimetypes
+import os
 from bot import app
 from pyrogram.client import Client
 from gallery_dl.job import DownloadJob
 from pyrogram.types.messages_and_media import Message
+from pyrogram.types import InputMediaPhoto, InputMediaVideo, InputMediaDocument
 from bot.filters.regex_filter import create_regex_filter
 from bot.lib.gallery_dl_hook import UploadGroupPostProcessor
 
@@ -9,9 +12,31 @@ INSTAGRAM_LINK = r"(?:https?://)?(?:www\.)?instagram\.com"
 
 is_instagram_link = create_regex_filter(INSTAGRAM_LINK)
 
+
+def mimetype_to_telegram(path: str):
+  (_type, _encoding) = mimetypes.guess_type(path)
+  if _type is None:
+    raise Exception(f'Mime Type cannot be guessed for path: {path}')
+
+  if _type.startswith('image'):
+    return InputMediaPhoto(path)
+  
+  if _type.startswith('video'):
+    return InputMediaVideo(path)
+    
+  raise Exception(f'Unhandled Mime Type: {_type} : {_encoding}')
+
 @app.on_message(filters=is_instagram_link)
 async def _(client: Client, msg: Message):
-  reply = await msg.reply_text('Handling Instagram Link', quote=True)
-  dl_job = DownloadJob(msg.text)
-  UploadGroupPostProcessor(dl_job, msg)
-  dl_job.run()
+  handling_reply = await msg.reply_text('Handling Instagram Link', quote=True) 
+  hook_job = UploadGroupPostProcessor(DownloadJob(msg.text))
+  downloaded_files = await hook_job.job_result()
+  
+  # files have been downloaded, send the files
+  input_media = list(map(mimetype_to_telegram, downloaded_files))
+  await msg.reply_media_group(media=input_media, quote=True)
+  await handling_reply.delete()
+
+  # clean up the files from filesystem
+  for file in downloaded_files:
+    os.remove(file)
